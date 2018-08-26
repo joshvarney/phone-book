076ed058f450ea3a7f5a52b43b2347c5d40eee86
Josh Varney <joshcainvarney@gmail.com>
2018-08-25 20:40:51 -0400

    still working on getting it to heroku

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'mysql2'
 require 'aws-sdk'
 enable :sessions
-load 'local_ENV.rb' if File.exist?('local_ENV.rb')
-client = Mysql2::Client.new(:username => ENV['RDS_USERNAME'], :password => ENV['RDS_PASSWORD'], :host => ENV['RDS_HOSTNAME'], :port => ENV['RDS_PORT'], :database => ENV['RDS_DB_NAME'], :socket => '/tmp/mysql.sock')
+# load 'local_ENV.rb' if File.exist?('local_ENV.rb')
+# client = Mysql2::Client.new(:username => ENV['RDS_USERNAME'], :password => ENV['RDS_PASSWORD'], :host => ENV['RDS_HOSTNAME'], :port => ENV['RDS_PORT'], :database => ENV['RDS_DB_NAME'], :socket => '/tmp/mysql.sock')
 
 get '/' do
 	erb :login_page, locals:{error: "", error2: ""}
